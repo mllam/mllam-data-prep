84c7058ec07b3e96ce061057dce6ff6b42fc3214
Leif Denby <leif@denby.eu>
2024-03-21 10:42:30 +0100

    add optional progress meter to cli

--- mllam_data_prep/__main__.py
+++ mllam_data_prep/__main__.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+from dask.diagnostics import ProgressBar
+
 from .create_dataset import main
 
 if __name__ == "__main__":
@@ -7,6 +9,12 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("config", help="Path to the config file", type=Path)
+    parser.add_argument(
+        "--show-progress", help="Show progress bar", action="store_true"
+    )
     args = parser.parse_args()
+
+    if args.show_progress:
+        ProgressBar().register()
 
     main(fp_config=args.config)

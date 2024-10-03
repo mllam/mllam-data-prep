35db55d2cfa843e54395c87c673a7221df5a37bc
Hauke Schulz <43613877+observingClouds@users.noreply.github.com>
2024-10-03 12:10:19 +0000

    add dask-distribution specific tests

---
+++ tests/test_distributed.py
@@ -0,0 +1,38 @@
+import importlib
+import tempfile
+
+import pytest
+import xarray as xr
+
+from mllam_data_prep.cli import call
+
+
+def call_wrapper(args):
+    with tempfile.TemporaryDirectory(suffix=".zarr") as tmpdir:
+        args.extend(["--output", tmpdir])
+        call(args)
+        _ = xr.open_zarr(tmpdir)
+
+
+def distributed():
+    """Check if dask.distributed is installed"""
+    try:
+        importlib.import_module("dask.distributed")
+
+        return True
+    except ImportError:
+        return False
+
+
+@pytest.mark.parametrize(
+    "args", [["example.danra.yaml", "--dask-distributed-local-core-fraction", "0.1"]]
+)
+def test_run_distributed(args):
+    if distributed():
+        call_wrapper(args)
+    else:
+        pytest.raises(
+            ModuleNotFoundError,
+            call_wrapper(args),
+            match="Currently dask.distrubuted is not installed",
+        )

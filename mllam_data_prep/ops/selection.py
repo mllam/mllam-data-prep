42ef339842de2e7c3936bc05780a2202859eeb31
Jacob Mathias Schreiner <masc@pf574y38.usr.dmi.dk>
2025-01-30 08:37:45 +0100

    add selection to input

--- mllam_data_prep/ops/selection.py
+++ mllam_data_prep/ops/selection.py
@@ -54,6 +54,7 @@ def select_by_kwargs(ds, **config_dict):
     xr.Dataset
         Dataset with the selection made
     """
+
     for coord, selection in config_dict.items():
         if coord not in ds.coords:
             raise ValueError(f"Coordinate {coord} not found in dataset")
@@ -73,6 +74,7 @@ def select_by_kwargs(ds, **config_dict):
             ds = ds.sel({coord: slice(sel_start, sel_end)})
 
             # check that the start and end are in the data
+            # Maybe this should be factored out as a unit test?
             coord_minmax = ds[coord].min().values, ds[coord].max().values
             if sel_start is not None and sel_start not in ds[coord].values:
                 raise ValueError(
@@ -100,6 +102,58 @@ def select_by_kwargs(ds, **config_dict):
                     raise ValueError(
                         f"Step size for coordinate {coord} is not the same as requested: {first_step} != {sel_step}"
                     )
+
+        elif isinstance(selection, list):
+            ds = ds.sel({coord: selection})
+        else:
+            raise NotImplementedError(
+                f"Selection for coordinate {coord} must be a list or a dict"
+            )
+    return ds
+
+
+def select_input_by_kwargs(ds, **config_dict):
+    """
+    Do `xr.Dataset.sel` on `ds` using the `config_dict` to select the coordinates, for each
+    entry in the dictionary, the key is the coordinate name and the value is the selection
+    to make, either given as 1) a list of values or a 2) dictionary with keys "start" and "end".
+    This functionally works like `xr.Dataset.sel` but can create slice objects for each
+    selection from the dictionary provided and also supports the use of ISO 8601 duration strings.
+
+    In future time interpolation and subsampling could be done here
+
+    Parameters
+    ----------
+    ds : xr.Dataset
+        Dataset to select from
+    config_dict : dict
+        Dictionary with the coordinate names as keys and the selection to make as values,
+        either a list of values or a dictionary with keys "start" and "end"
+        (and optionally "step" for the slice object)
+
+    Returns
+    -------
+    xr.Dataset
+        Dataset with the selection made
+    """
+    for coord, selection in config_dict.items():
+        if coord not in ds.coords:
+            raise ValueError(f"Coordinate {coord} not found in dataset")
+        if isinstance(selection, Range):
+            if selection.start is None and selection.end is None:
+                raise ValueError(
+                    f"Selection for coordinate {coord} must have either 'start' and 'end' given"
+                )
+            sel_start = _normalize_slice_startstop(selection.start)
+            sel_end = _normalize_slice_startstop(selection.end)
+
+            assert sel_start != sel_end, "Start and end cannot be the same"
+
+            ds = ds.sel({coord: slice(sel_start, sel_end)})
+
+            assert (
+                len(ds[coord]) > 0
+            ), f"You have selected an empty range {sel_start}:{sel_end} for coordinate {coord}"
 
         elif isinstance(selection, list):
             ds = ds.sel({coord: selection})

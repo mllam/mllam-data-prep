60e7f44bc21f62f47982e5e9e1e9590f23560b21
Jacob Mathias Schreiner <masc@pf574y38.usr.dmi.dk>
2025-01-30 08:28:52 +0100

    add teest

---
+++ tests/test_selection.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+
+import pytest
+import xarray as xr
+import yaml
+
+import mllam_data_prep as mdp
+
+
+@pytest.fixture
+def ds():
+    fp = "https://mllam-test-data.s3.eu-north-1.amazonaws.com/height_levels.zarr"
+    return xr.open_zarr(fp)
+
+
+def test_range_slice_within_range(ds):
+    x_start = -50000
+    x_end = -40000
+    y_start = -600000
+    y_end = -590000
+    coord_ranges = {
+        "x": mdp.config.Range(start=x_start, end=x_end),
+        "y": mdp.config.Range(start=y_start, end=y_end),
+    }
+
+    ds = mdp.ops.selection.select_input_by_kwargs(ds, **coord_ranges)
+    assert ds.x.min() >= x_start
+    assert ds.x.max() <= x_end
+    assert ds.y.min() >= y_start
+    assert ds.y.max() <= y_end
+
+    ds
+
+
+@pytest.mark.parametrize("x_start, x_end", ([-50000, -51000], [0, 500000]))
+def test_error_on_empty_range(ds, x_start, x_end):
+    y_start = -600000
+    y_end = -590000
+    coord_ranges = {
+        "x": mdp.config.Range(start=x_start, end=x_end),
+        "y": mdp.config.Range(start=y_start, end=y_end),
+    }
+
+    with pytest.raises(AssertionError):
+        ds = mdp.ops.selection.select_input_by_kwargs(ds, **coord_ranges)

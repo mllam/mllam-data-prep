9e2d746844eb008b65cd2c1a6052b4f5715d9acd
Jacob Mathias Schreiner <masc@pf574y38.usr.dmi.dk>
2025-02-06 13:42:44 +0100

    Revert "remove coord_ranges from dim_mapping"
    This reverts commit 6bac0a44d49d66146365e87d23fce6c43a007591.

--- mllam_data_prep/config.py
+++ mllam_data_prep/config.py
@@ -235,6 +235,7 @@ class InputDataset:
     variables: Optional[Union[List[str], Dict[str, Dict[str, ValueSelection]]]] = None
     derived_variables: Optional[Dict[str, DerivedVariable]] = None
     attributes: Optional[Dict[str, Any]] = field(default_factory=dict)
+    coord_ranges: Dict[str, Range] = None
 
 
 @dataclass

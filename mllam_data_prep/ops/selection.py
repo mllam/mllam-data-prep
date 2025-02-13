570bc65cd702f7fa4d9621c5a8a1e7a831aa3b3b
Jacob Mathias Schreiner <masc@pf574y38.usr.dmi.dk>
2025-02-13 18:21:11 +0100

    is always range

--- mllam_data_prep/ops/selection.py
+++ mllam_data_prep/ops/selection.py
@@ -56,31 +56,25 @@ def select_by_kwargs(ds, **coord_ranges):
     """
 
     for coord, selection in coord_ranges.items():
-        if isinstance(selection, Range):
-            if selection.start is None and selection.end is None:
-                raise ValueError(
-                    f"Selection for coordinate {coord} must have either 'start' and 'end' given"
-                )
-            sel_start = str_to_datetime(selection.start)
-            sel_end = str_to_datetime(selection.end)
-            sel_step = str_to_timedelta(selection.step)
+        sel_start = str_to_datetime(selection.start)
+        sel_end = str_to_datetime(selection.end)
+        sel_step = str_to_timedelta(selection.step)
 
-            assert sel_start != sel_end, "Start and end cannot be the same"
+        assert sel_start != sel_end, "Start and end cannot be the same"
 
-            if coord == "time" and sel_step is not None:
-                warnings.warn(
-                    "Step size for time coordinate is not supported and is ignored"
-                )
-                sel_step = None
-
-            ds = ds.sel({coord: slice(sel_start, sel_end, sel_step)})
-
-            assert (
-                len(ds[coord]) > 0
-            ), f"You have selected an empty range {sel_start}:{sel_end} for coordinate {coord}"
-
+        if coord == "time" and sel_step is not None:
+            warnings.warn(
+                "Step size for time coordinate is not supported and is ignored"
             )
+            sel_step = None
+
         check_selection(ds, coord, sel_start, sel_end)
+        ds = ds.sel({coord: slice(sel_start, sel_end, sel_step)})
+
+        assert (
+            len(ds[coord]) > 0
+        ), f"You have selected an empty range {sel_start}:{sel_end} for coordinate {coord}"
+
     return ds
 
 def check_selection(ds, coord, sel_start, sel_end):
